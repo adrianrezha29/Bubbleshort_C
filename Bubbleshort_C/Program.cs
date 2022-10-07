cec91a7fd57b0f1a5951209692028c65a2d4b479
adrianrezha29 <adrianrezha29@gmail.com>
2022-10-07 17:34:22 +0700

    menampilkan array yang tersusun

--- Bubbleshort_C/Program.cs
+++ Bubbleshort_C/Program.cs
@@ -35,6 +35,20 @@ namespace Bubble_Short
                 a[i] = Int32.Parse(s1);
             }
         }
-        public void display ()
-        { 
-             
+        public void display()
+        {
+            //Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Element array yang telah tersusun");
+            Console.WriteLine("---------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 0; i < n; i++)
+        }
